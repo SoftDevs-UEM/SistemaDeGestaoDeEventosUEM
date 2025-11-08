893a5a356e764c6fbdb3e30f1be05f70d4601916
Ussene C. Matato <ussene.c.matato@hotmail.com>
2025-11-08 12:28:47 +0200

    upd: file system config

--- frontend/src/pages/CadastrarPromotor.tsx
+++ frontend/src/pages/CadastrarPromotor.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
-import './cadastrarPromotor.css';
+import './CadastrarPromotor.css';
 
 export default function CadastrarPromotor() {
   const navigate = useNavigate();
